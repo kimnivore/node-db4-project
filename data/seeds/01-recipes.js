d771ff914d5dc0ea4dd1ac6f0e730dee9fa27de4
Kim Nguyen <kimnivore@gmail.com>
2022-02-17 15:10:56 -0800

    seeds

--- data/seeds/01-recipes.js
+++
@@ -1,9 +0,0 @@
-exports.seed = async function(knex) {
-  await knex('recipes').truncate();
-  return knex('recipes').insert([
-    {recipe_name: 'pasta', steps: 'boil pasta' },
-    {recipe_name: 'pasta', steps: 'make sauce' },
-    {recipe_name: 'pasta', steps: 'combine pasta' },
-    
-  ]);
-};
--- data/seeds/02-steps.js
+++
@@ -1,9 +0,0 @@
-
-exports.seed = async function(knex) {
-  await knex('steps').truncate();
-  return knex('steps').insert([
-    {step_number: '1', step_instructions: 'boil pasta' },
-    
-    
-  ]);
-};
